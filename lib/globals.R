56e4c922a1719f0223f4dbb7fc3545350509efcf
Navy Cheng <navych@126.com>
2018-09-14 15:27:16 +0800

    Learn ProjectTemplate

--- lib/globals.R
+++ lib/globals.R
@@ -5,5 +5,23 @@ add.config(
 
 # Add project specific configuration that can be overridden from load.project()
 add.config(
-  apply.override = TRUE
+  apply.override = TRUE,
+  bioMart = 'data/mart_export.txt',
+  diffRnaFile = 'reports/diffGenes.csv',
+  lncRNA = c('lincRNA',
+              'bidirectional_promoter_lncRNA',
+              '3prime_overlapping_ncRNA',
+              'macro_lncRNA',
+              'antisense',
+              'sense_overlapping',
+              'sense_intronic'),
+  PCGs = c('protein_coding',
+            'IG_C_gene',
+            'IG_J_gene',
+            'TR_D_gene',
+            'TR_V_gene',
+            'TR_J_gene',
+            'IG_D_gene',
+            'IG_V_gene',
+            'TR_C_gene')
 )
---
+++ src/lncRNA-cis.R
@@ -0,0 +1,5 @@
+library(ProjectTemplate)
+
+library(data.table)
+
+load.project()
