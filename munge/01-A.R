73fed15665b6610aff40142fada426eb0f51f96f
Navy Cheng <navych@126.com>
2018-09-15 16:12:02 +0800

    Add lncRNA cis analysis

--- munge/01-A.R
+++ munge/01-A.R
@@ -4,3 +4,4 @@ diffLncRna <- filter(diffRna, GeneType %in% config$lncRNA)
 diffPcg <- filter(diffRna, GeneType %in% config$PCGs)
 diffLncRnaAndPcg <- filter(diffRna, GeneType %in% c(config$lncRNA, config$PCGs))
 
+bioMart <- distinct(mart.export, Gene.stable.ID, .keep_all = TRUE)
