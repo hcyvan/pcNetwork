93137c55aa4f62c0a57895ff43c5eb0c72458f08
Navy Cheng <navych@126.com>
2018-11-09 16:41:58 +0800

    Add

--- src/04_tf_enrichment.R
+++ src/04_tf_enrichment.R
@@ -4,28 +4,81 @@ library(org.Hs.eg.db)
 
 source('./lib/globals.R')
 source('./lib/helpers.R')
+diff.gene <- helper.get.lncRNA.PCG()
+pcg <- diff.gene %>% filter(GeneType%in%config$PCGs) %>% dplyr::select(id=GeneID, fc=logFC) %>% arrange(desc(fc)) 
 
-modules <- read.delim('./data/diff.qlf.2877.wgcna.color.csv', sep = ',',header = TRUE, stringsAsFactors = FALSE)
 
-bg1 <- read.delim('./data/ENCODE_TF_ChIP-seq_2015.handle.txt', sep = ',',header = TRUE, stringsAsFactors = FALSE)
-bg2 <- read.delim('./data/ChEA_2016.handle.txt', sep = '\t',header = TRUE, stringsAsFactors = FALSE)
+# modules <- read.delim('./data/diff.qlf.2877.wgcna.color.csv', sep = ',',header = TRUE, stringsAsFactors = FALSE)
+
+bg1 <- read.delim('./data/enricher/ENCODE_TF_ChIP-seq_2015.handle.txt', sep = ',',header = TRUE, stringsAsFactors = FALSE)
+bg2 <- read.delim('./data/enricher/ChEA_2016.handle.txt', sep = '\t',header = TRUE, stringsAsFactors = FALSE)
+bg3 <- read.delim('./data/enricher/TRANSFAC_and_JASPAR_PWMs.handle.txt', sep = '\t',header = TRUE, stringsAsFactors = FALSE)
 
 bg1 <- bg1[,c('tf','symbol')]
-bg3 <- rbind(bg1,bg2)
+bg0 <- rbind(bg1,bg2,bg3)
 
 bg1 <- distinct(bg1)
 bg2 <- distinct(bg2)
 bg3 <- distinct(bg3)
-
-barplot(sort(table(bg$tf)))
-barplot(sort(table(bg$symbol)))
+bg0 <- distinct(bg0)
 
 dim(bg1)
 dim(bg2)
 dim(bg3)
-unique(bg1$tf)
-unique(bg2$tf)
-intersect(bg1$tf, bg2$tf)
+dim(bg0)
+
+length(unique(bg1$tf))
+length(unique(bg2$tf))
+length(unique(bg3$tf))
+length(unique(bg0$tf))
+
+barplot(sort(table(bg0$tf)))
+barplot(sort(table(bg0$symbol)))
+
+# ---------------------------------------------- filter bg
+library(biomaRt)
+listMarts()
+ensembl=useMart("ENSEMBL_MART_ENSEMBL")
+listDatasets(ensembl)
+ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
+ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
+filters = listFilters(ensembl)
+attributes = listAttributes(ensembl)
+searchAttributes(mart = ensembl, pattern = "ensembl")
+biomart.symbol.biotype = getBM(attributes = c('ensembl_gene_id','hgnc_symbol', 'gene_biotype'), mart = ensembl)
+save(biomart.symbol.biotype, file = './cache/biomart.symbol.biotype.rda')
+filter(biomart.symbol.biotype, gene_biotype%in%config$PCGs) -> biomart.pcgs
+
+bg0.new <- filter(bg0, symbol%in%biomart.pcgs$hgnc_symbol)
+bg1.new <- filter(bg1, symbol%in%biomart.pcgs$hgnc_symbol)
+bg2.new <- filter(bg2, symbol%in%biomart.pcgs$hgnc_symbol)
+bg3.new <- filter(bg3, symbol%in%biomart.pcgs$hgnc_symbol)
+
+# -------------------------------------------
+
+###################################################### Diff Gene Enricher
+colorEnricherAll <- function(bg, pvalueCutoff=0.05) {
+  tf2gene <- bg[,c('tf','symbol')]
+  geneList <- inner_join(pcg, biomart.pcgs, by=c('id'='ensembl_gene_id'))$hgnc_symbol
+  enricher.all <- enricher(geneList, TERM2GENE = tf2gene, pvalueCutoff = pvalueCutoff, minGSSize=NULL, maxGSSize=NULL)
+  list(detail=enricher.all, tfs=as.data.frame(enricher.all)$ID)
+}
+enricher.all.bg1.001 <- colorEnricherAll(bg1,0.01)
+enricher.all.bg2.001 <- colorEnricherAll(bg2,0.01)
+enricher.all.bg3.001 <- colorEnricherAll(bg3,0.01)
+enricher.all.bg0.001 <- colorEnricherAll(bg0,0.01)
+enricher.all.bg0.new.001 <- colorEnricherAll(bg0.new,0.01)
+
+
+enricher.all.bg1 <- colorEnricherAll(bg1)
+enricher.all.bg2 <- colorEnricherAll(bg2)
+enricher.all.bg3 <- colorEnricherAll(bg3)
+enricher.all.bg0 <- colorEnricherAll(bg0)
+enricher.all.bg0.new <- colorEnricherAll(bg0.new) ### Use this
+
+write.csv(enricher.all.bg0.new, file = './data/enricher.all.bg0.new.csv', row.names = FALSE)
+
+####################################################### WGCNA Moudle
 ## ------------------------------- over-representation analysis ----------------------------------
 ### ---------------- for each color
 colorEnricher <- function(c, bg, pvalueCutoff) {
@@ -66,10 +119,12 @@ colorEnricherAll <- function(bg, pvalueCutoff=0.05) {
 enricher.all.bg1.001 <- colorEnricherAll(bg1,0.01)
 enricher.all.bg2.001 <- colorEnricherAll(bg2,0.01)
 enricher.all.bg3.001 <- colorEnricherAll(bg3,0.01)
+enricher.all.bg0.001 <- colorEnricherAll(bg3,0.01)
 
 enricher.all.bg1 <- colorEnricherAll(bg1)
 enricher.all.bg2 <- colorEnricherAll(bg2)
 enricher.all.bg3 <- colorEnricherAll(bg3)
+enricher.all.bg0 <- colorEnricherAll(bg0)
 
 ### -------- export
 as.data.frame(enricher.bg3.001$detail$brown)
@@ -134,6 +189,7 @@ colorGSEAAll <- function(bg) {
 gseaer.all.bg1 <- colorGSEAAll(bg1)
 gseaer.all.bg2 <- colorGSEAAll(bg2)
 gseaer.all.bg3 <- colorGSEAAll(bg3)
+gseaer.all.bg0 <- colorGSEAAll(bg0)
 
 
 
