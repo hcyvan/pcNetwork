e808edbe05ba6453039893be18b7716b91a21432
Navy Cheng <navych@126.com>
2018-12-10 22:54:01 +0800

    add multicor

--- src/02_correlation.R
+++ src/02_correlation.R
@@ -15,20 +15,104 @@ genes.anno <- left_join(diff.gene, biomart, by=c('GeneID'='Gene.stable.ID')) %>%
          geneEnd = Transcript.end..bp., 
          chromosome = Chromosome.scaffold.name)
 
+
+multicor <- function(data, method= c('pearson', 'kendall', 'spearman'), rds=NA, rewrite=FALSE, verbose=TRUE) {
+  # Data structure like this:
+  #
+  #       sample1  sample2  sample3
+  # gene1   xx      xx      xx
+  # gene2   xx      xx      xx
+  # gene3   xx      xx      xx
+  #
+  # Return:
+  #         v1, v2, r, p.value, FDR
+  
+  if(!is.na(rds)) {
+    if(file.exists(rds) && !rewrite) {
+      message(paste('Load data from', rds))
+      return(readRDS(rds))
+    }
+  }
+  
+  method <- match.arg(method)
+  data <- as.matrix(data)
+  r <- matrix(nrow=nrow(data),ncol=nrow(data))
+  if (verbose) {
+    message(paste('Calculating:', nrow(data),'rounds needed!'))
+  }
+  i <- 0
+  p <- apply(data,1,function(x){
+    i<<-i+1
+    t0 <- Sys.time()
+    if (verbose) {
+      cat(paste0(i, ' '))
+    }
+    j<-0
+    apply(data,1,function(y){
+      j<<-j+1
+      if (i > j) {
+        test <- cor.test(x,y,method = method)
+        r[j,i] <<- test$estimate
+        test$p.value
+      } else {
+        NaN
+      }
+    })
+  })
+  if (verbose) {
+    cat('\n')
+  }
+  dimnames(r) <- dimnames(p)
+  list(r,p)
+  r.melt <- reshape2::melt(r) %>% filter(!is.na(value))
+  p.melt <- reshape2::melt(p) %>% filter(!is.na(value))
+  ret <- data.frame(v1=r.melt$Var1, v2=r.melt$Var2, r=r.melt$value, p.value=p.melt$value, FDR=p.adjust(p.melt$value, method = 'BH'))
+  if (!is.na(rds)) {
+    saveRDS(ret, rds)
+  }
+  ret
+}
+
+cor.pairs <- multicor(genes.fpkm, rds = './cache/test.rds')
+
+
+
+annoPairs <- function(cor.pairs, annotation) {
+  g1 <- annotation[match(cor.pairs$v1, annotation$GeneID),]
+  g2 <- annotation[match(cor.pairs$v2, annotation$GeneID),]
+  data.frame(v1=cor.pairs$v1,
+             v2=cor.pairs$v2,
+             type1=g1$GeneType,
+             type2=g2$GeneType,
+             r=cor.pairs$r,
+             p.value=cor.pairs$p.value,
+             FDR=cor.pairs$FDR,
+             scaffold1=g1$chromosome,
+             scaffold2=g2$chromosome,
+             distance=ifelse(g1$chromosome==g2$chromosome, g1$tss-g2$tss,Inf))
+}
+
+system.time(cor.pairs.info <- annoPairs(cor.pairs, genes.anno))
+saveRDS(cor.pairs.info, file = './cache/cor.pairs.info.rds')
+
+
+
+############################################################### Old Version
 ## Cor pvalue
 ### pvalue
 cor.test.multi <- function(data) {
   data <- as.matrix(data)
   i <- 0
   diff.pvalue <- apply(data,1,function(x){
-    print(i<<-i+1)
+    i<<-i+1
+    cat(paste0(i, ' '))
     apply(data,1,function(y){
       cor.test(x,y)$p.value
     })
   })
 }
 
-# system.time(cor.pvalue <- cor.test.multi(genes.fpkm))
+system.time(cor.pvalue <- cor.test.multi(genes.fpkm))
 # saveRDS(cor.pvalue, './cache/diff.qlf.2877.cor.pvalue.rds')
 cor.pvalue <- readRDS('./cache/diff.qlf.2877.cor.pvalue.rds')
 
