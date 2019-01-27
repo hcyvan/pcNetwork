58d7d44da07ba86a2359ebcb6d6d6617925e7fe7
Navy Cheng <navych@126.com>
2019-01-27 17:08:30 +0800

    update

--- R/00_TF_targets.R
+++ R/00_TF_targets.R
@@ -7,61 +7,29 @@ tf2gene <- function(pairs) {
 }
 
 ### Fimo TSS
-read.fimo.460.tss <- function() {
-    ret <- data.table()
-    for(i in 1:4) {
-        fimo <- fread(paste0('./data/fimo.460.tss/fimo.460.tss.1000.', i, '.tsv'), check.names = TRUE)
-        sequence_name <- str_split(fimo$sequence_name, '\\|', simplify = TRUE)
-        gene <- sequence_name[,1]
-        transcript <- sequence_name[,2]
-        chromosome <- sequence_name[,3]
-        fimo[,c('gene','transcript','chromosome'):=list(gene, transcript, chromosome)]
-        fimo_uniq <- unique(fimo, by=c('motif_id', 'start', 'strand', 'chromosome'))
-        ret <- rbind(ret, fimo_uniq)
-    }
-    ret
-}
-fimo <- read.fimo.460.tss()
-fimo.tss.set <- fimo[p.value<1e-4, .(.N), by=.(motif_alt_id, gene)][,.(tf=motif_alt_id, gene)]
-fimo.tss.set[,.(.N),by=(tf)]
+fimo.tss.1 <- fread(paste0('./data/fimo.460.tss/fimo.460.tss.1000.1.tsv'), check.names = TRUE)
+fimo.tss.2 <- fread(paste0('./data/fimo.460.tss/fimo.460.tss.1000.2.tsv'), check.names = TRUE)
+fimo.tss.3 <- fread(paste0('./data/fimo.460.tss/fimo.460.tss.1000.3.tsv'), check.names = TRUE)
+fimo.tss.4 <- fread(paste0('./data/fimo.460.tss/fimo.460.tss.1000.4.tsv'), check.names = TRUE)
+fimo.tss <- rbind(fimo.tss.1, fimo.tss.2, fimo.tss.3, fimo.tss.4)
+name <- str_split(fimo.tss$sequence_name, '\\|', simplify = TRUE)
+fimo.tss[,c('gene','transcript','chromosome'):=list(name[,1], name[2], name[,3])]
+fimo.tss.set <- unique(fimo.tss[p.value<=1e-5,.(tf=motif_alt_id, gene=gene)])
 saveRDS(as.data.frame(fimo.tss.set), './cache/fimo.tss.set.rds')
-tf2gene.fimo.tss <- tf2gene(fimo.tss.set)
-
 ### Fimo GSS
-fimo.gss <- fread('./data/fimo.460.gss.tsv', stringsAsFactors = F)
-fimo.gss.set <- fimo.gss[, .(tf=motif_alt_id, gene=sequence_name)]
-
-get.tf.2.PCG.from.fimo <- function() {
-  env = globalenv()
-  key = '.tf.2.PCG'
-  if (exists(key, envir = env)) {
-    get(key, envir = env)
-  } else {
-    fimo <- read.delim('./data/enricher/fimo.460.2230.tsv', stringsAsFactors = F)
-    fimo <- filter(fimo, motif_alt_id!='')
-    tf.2.PCG <- lapply(split(fimo,as.vector(fimo$motif_alt_id)), function(x){unique(as.vector(x$sequence_name))})
-    assign(key, tf.2.PCG, envir = env)
-    tf.2.PCG
-  }
-}
-tf2gene.fimo.gss <- get.tf.2.PCG.from.fimo()
-saveRDS(as.data.frame(fimo.tss.set), './cache/fimo.gss.list.rds')
+fimo.gss <- fread('./data/fimo.460.gss.tsv', stringsAsFactors = F, check.names = TRUE)
+fimo.gss.set <- unique(fimo.gss[p.value<1e-5, .(tf=motif_alt_id, gene=sequence_name)])
+saveRDS(as.data.frame(fimo.tss.set), './cache/fimo.gss.set.rds')
 ### TRUST
 trrust<-fread('./data/trrust_rawdata.human.tsv')
-trrust.set <- trrust[,.(tf=V1,gene=pf.symbol2emsembl(V2))]
-trrust.set[,.(.N),by=(tf)]
+trrust.set <- unique(trrust[,.(tf=V1,gene=pf.symbol2emsembl(V2))])
 saveRDS(as.data.frame(trrust.set), './cache/trrust.set.rds')
-tf2gene.trrust <- tf2gene(trrust.set)
-
-
 ### Gtrd
-gtrd <- fread('./data/Homo_sapiens_meta_clusters.interval', check.names = TRUE)
-gtrd.mid <- gtrd[,.(chrom=str_sub(X.CHROM, 4), start=START, end=END, len=END-START, tf=tfTitle)]
-
-biomart.set <- as.data.table(pf.get.biomart())
-biomart.set <- biomart.set[chromosome_name%in%gtrd.mid$chrom,][,.(gene=ensembl_gene_id, transcript=ensembl_transcript_id, tss=transcription_start_site, up=transcription_start_site-1000,chrom=chromosome_name)]
-biomart.map<-split(biomart.set, by = 'chrom')
-
+#gtrd <- fread('./data/Homo_sapiens_meta_clusters.interval', check.names = TRUE)
+#gtrd.mid <- gtrd[,.(chrom=str_sub(X.CHROM, 4), start=START, end=END, len=END-START, tf=tfTitle)]
+#biomart.set <- as.data.table(pf.get.biomart())
+#biomart.set <- biomart.set[chromosome_name%in%gtrd.mid$chrom,][,.(gene=ensembl_gene_id, transcript=ensembl_transcript_id, tss=transcription_start_site, up=transcription_start_site-1000,chrom=chromosome_name)]
+#biomart.map<-split(biomart.set, by = 'chrom')
 #i <- 0
 #total <- nrow(gtrd.mid)
 #genes<-mapply(function(chrom, start, end){
@@ -81,11 +49,12 @@ gtrd.mid <- readRDS('./cache/gtrd.mid.rds')
 gtrd.set<-unique(gtrd.mid[!is.na(gene), .(tf, gene)])
 saveRDS(as.data.frame(gtrd.set), './cache/gtrd.set.rds')
 gtrd.set <- as.data.table(readRDS('./cache/gtrd.set.rds'))
-gtrd.set[,.(.N),by=(tf)]
-tf2gene.gtrd <- tf2gene(gtrd.set)
 ##################
-par(mfrow=c(2,2))
-plot(sort(sapply(tf2gene.fimo.tss, length)), ylab = 'pcg number', xlab = 'fimo1')
-plot(sort(sapply(tf2gene.fimo.gss, length)), ylab = 'pcg number', xlab = 'fimo0')
-plot(sort(sapply(tf2gene.trrust, length)), ylab = 'pcg number', xlab = 'trrust')
-plot(sort(sapply(tf2gene.gtrd, length)), ylab = 'pcg number', xlab = 'gtrd')
+library(ggplot2)
+library(cowplot)
+
+fimo.tss.hist <- ggplot(fimo.tss.set[,.(.N), by=(tf)], aes(x=N))+geom_histogram(binwidth = 100)
+fimo.gss.hist <- ggplot(fimo.gss.set[,.(.N), by=(tf)], aes(x=N))+geom_histogram(binwidth = 100)
+trrust.hist <- ggplot(trrust.set[,.(.N),by=(tf)], aes(x=N))+geom_histogram(binwidth = 1)
+gtrd.hist <- ggplot(gtrd.set[,.(.N),by=(tf)], aes(x=N))+geom_histogram(binwidth = 100)
+plot_grid(fimo.tss.hist, fimo.gss.hist, trrust.hist, gtrd.hist, labels = c(1,2,3,4))
